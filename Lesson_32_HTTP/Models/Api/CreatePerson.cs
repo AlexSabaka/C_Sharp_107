108c89d63f7b182513a64c78dde8f424a60043d7
Alex Sabaka <alex.love.broadcast@gmail.com>
2023-11-15 21:42:18 +0200

    Lesson 32 HTTP

---
+++ Lesson_32_HTTP/Models/Api/CreatePerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleServer.Models.Api
+{
+    public class CreatePersonRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public bool Verified { get; set; }
+        public string Email { get; set; }
+        public byte Age { get; set; }
+    }
+
+    public class CreatePersonResponse
+    {
+        public int Id { get; set; }
+    }
+}
